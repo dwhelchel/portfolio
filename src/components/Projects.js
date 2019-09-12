be1f9bbdde79bebee01b701efc354376554f7c28
Dakota Whelchel <dakotabwhelchel@gmail.com>
2019-09-12 05:08:46 -0700

    added project page card layout

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import Card from './Card';
 import '../css/Projects.css';
 
 class Projects extends Component {
     render() {
         return(
-            <div className="projects" id="projects"></div>
+            <div className="projects" id="projects">
+                <div className="projects-container">
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                </div>
+            </div>
         );
     }
 }
