98f75fe1844c93a9077598dac34d4a502338b488
Dakota Whelchel <dakotabwhelchel@gmail.com>
2019-09-12 03:12:51 -0700

    added background and fixed header

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,12 @@ import '../css/Main.css';
 class Main extends Component {
     render() {
         return(
-            <div className="main" id="main"></div>
+            <div className="main" id="main">
+                <div className="main-container">
+                    <h1>Hi, I'm Dakota</h1>
+                    <p>Nice to meet you</p>
+                </div>
+            </div>
         );
     }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,54 +3,48 @@ import { Link } from 'react-scroll';
 import '../css/Navbar.css';
 
 class Navbar extends Component {
-
-    constructor() {
-        super();
-        this.state = {
-            number: 0
-        }
-    }
-
     render() {
         return(
             <div className="navbar">
-                <div className="container">
-                    <Link 
-                        activeClass="active"
-                        to="main"
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration={500}
-                        className="link"
-                    >Main</Link>
-                    <Link 
-                        activeClass="active"
-                        to="projects"
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration={500}
-                        className="link"
-                    >Projects</Link>
-                    <Link 
-                        activeClass="active"
-                        to="about"
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration={500}
-                        className="link"
-                    >About</Link>
-                    <Link 
-                        activeClass="active"
-                        to="contact"
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration={500}
-                        className="link"
-                    >Contact</Link>
+                <div className="navbar-container">
+                    <div className="link-container">
+                        <Link 
+                            activeClass="active"
+                            to="main"
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration={500}
+                            className="link"
+                        >Main</Link>
+                        <Link 
+                            activeClass="active"
+                            to="projects"
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration={500}
+                            className="link"
+                        >Projects</Link>
+                        <Link 
+                            activeClass="active"
+                            to="about"
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration={500}
+                            className="link"
+                        >About</Link>
+                        <Link 
+                            activeClass="active"
+                            to="contact"
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration={500}
+                            className="link"
+                        >Contact</Link>
+                    </div>
                 </div>
             </div>
         );
